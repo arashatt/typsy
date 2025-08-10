46fa831a264ff1942a5d8ab52df0d2c557d16d42
Arash <attariarash@gmail.com>
2025-08-10 09:25:11 +0000

    dependencies updated

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+	  server: {
+    host: '0.0.0.0', // listen on all IPs
+    port: 3000,      // optional, choose your port
+  },
     plugins: [    tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
